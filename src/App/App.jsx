a351a763fe1322aed88167d07d3bca866256ec20
tictools <jaumevillarreal@gmail.com>
2021-07-25 08:09:15 +0200

    Add data fetch in componentDidMount

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -3,55 +3,42 @@ import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import Item from "../components/Item/Item";
 import List from "../components/List/List";
+import { products } from "../common/products";
 import "./App.css";
 
-function App() {
-  const products = [
-    {
-      product: "flash t-shirt",
-      price: 27.5,
-      category: "t-shirts",
-      bestSeller: false,
-      image:
-        "https://images-na.ssl-images-amazon.com/images/I/61ZipyCaAKL._AC_UX385_.jpg",
-      onSale: true,
-    },
-    {
-      product: "batman t-shirt",
-      price: 22.5,
-      category: "t-shirts",
-      bestSeller: true,
-      image:
-        "https://s1.thcdn.com/productimg/1600/1600/11676326-1444552242012324.png",
-      onSale: false,
-    },
-    {
-      product: "superman hat",
-      price: 13.9,
-      category: "hats",
-      bestSeller: true,
-      image:
-        "https://banner2.kisspng.com/20180429/rqe/kisspng-baseball-cap-superman-logo-batman-hat-5ae5ef317f8366.9727520615250184175223.jpg",
-      onSale: false,
-    },
-  ];
+// https://barcelonacodeschool.com/files/reactB1products.json')
 
-  const bestSellers = products.filter((product) => product.bestSeller);
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [],
+    };
+  }
 
-  return (
-    <div className="app__wrapper">
-      <Header />
-      {products ? (
-        <main className="app__main">
-          <div className="products__container">
-            <List products={products} />
-            <List products={bestSellers} />
-          </div>
-        </main>
-      ) : null}
-      <Footer />
-    </div>
-  );
+  componentDidMount() {
+    fetch("https://barcelonacodeschool.com/files/reactB1products.json")
+      .then((response) => response.json())
+      .then((data) => this.setState({ products: data.products }));
+  }
+
+  render() {
+    const { products } = this.state;
+    const bestSellers = products.filter((product) => product.bestSeller);
+
+    return (
+      <div className="app__wrapper">
+        <Header />
+        {products ? (
+          <main className="app__main">
+            <div className="products__container">
+              <List label="products" products={products} />
+              <List label="best-sellers" products={bestSellers} />
+            </div>
+          </main>
+        ) : null}
+        <Footer />
+      </div>
+    );
+  }
 }
-
-export default App;
