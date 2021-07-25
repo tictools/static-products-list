c18ac3a0ea3c0419a099bb8700d7356c86a78f53
tictools <jaumevillarreal@gmail.com>
2021-07-25 08:08:33 +0200

    Move products array to common

---
+++ src/common/products.js
@@ -0,0 +1,29 @@
+export const products = [
+  {
+    product: "flash t-shirt",
+    price: 27.5,
+    category: "t-shirts",
+    bestSeller: false,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/61ZipyCaAKL._AC_UX385_.jpg",
+    onSale: true,
+  },
+  {
+    product: "batman t-shirt",
+    price: 22.5,
+    category: "t-shirts",
+    bestSeller: true,
+    image:
+      "https://s1.thcdn.com/productimg/1600/1600/11676326-1444552242012324.png",
+    onSale: false,
+  },
+  {
+    product: "superman hat",
+    price: 13.9,
+    category: "hats",
+    bestSeller: true,
+    image:
+      "https://banner2.kisspng.com/20180429/rqe/kisspng-baseball-cap-superman-logo-batman-hat-5ae5ef317f8366.9727520615250184175223.jpg",
+    onSale: false,
+  },
+];
